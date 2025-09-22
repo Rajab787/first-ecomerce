97546e8ea61e501b2100964217147a4110546ec9
Rajab787 <rajab10312@gmail.com>
2025-09-22 19:54:20 +0500

    bg white

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body className="bg-white" >
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
