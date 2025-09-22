f8fa08cd24c7228b6c8f31fafab9271c57bb6db3
Rajab787 <rajab10312@gmail.com>
2025-09-22 16:07:39 +0500

    fix bugs

--- src/app/components/Product/ProductData.jsx
+++ src/app/components/Product/ProductData.jsx
@@ -39,28 +39,28 @@ export const ProductData = {
         {
             id: 6,
             img: "Products/item-6.jpg",
-            productName: "Women's Boots Shoes Maca",
+            productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Classic ankle boots that pair well with any outfit."
         },
         {
             id: 7,
             img: "Products/item-7.jpg",
-            productName: "Women's Boots Shoes Maca",
+            productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Comfortable women’s boots with a fashionable edge."
         },
         {
             id: 8,
             img: "Products/item-8.jpg",
-            productName: "Women's Boots Shoes Maca",
+            productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Premium boots offering both style and durability."
         },
         {
             id: 9,
             img: "Products/item-9.jpg",
-            productName: "Women's Boots Shoes Maca",
+            productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Elegant boots designed to elevate your fashion look."
         },
--- src/app/men/MenData.jsx
+++ src/app/men/MenData.jsx
@@ -4,12 +4,14 @@ export const MenData= {
        
        
         {
+            id: 3,
             img: "Products/item-3.jpg",
             productName: "Men's Taja Commissioner",
             price: 139,
             description: "Durable men’s formal shoes with a sleek modern look."
         },
         {
+            id: 4,
             img: "Products/item-4.jpg",
             productName: "Russ Men's Sneakers",
             price: 139,
@@ -17,24 +19,28 @@ export const MenData= {
         },
       
         {
+            id: 6,
             img: "Products/item-6.jpg",
             productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Classic ankle boots that pair well with any outfit."
         },
         {
+            id: 7,
             img: "Products/item-7.jpg",
             productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Comfortable women’s boots with a fashionable edge."
         },
         {
+            id: 8,
             img: "Products/item-8.jpg",
             productName: "Men's Boots Shoes Maca",
             price: 139,
             description: "Premium boots offering both style and durability."
         },
         {
+            id: 9,
             img: "Products/item-9.jpg",
             productName: "Men's Boots Shoes Maca",
             price: 139,
